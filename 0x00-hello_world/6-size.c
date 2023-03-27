c6fd205b28de6f6cbeb4273426de4ee210b06fcf
NGABO Right Bruno <ngaborightbruno11@gmail.com>
2023-03-27 16:48:32 +0000

    print size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+/**
+ * main - intranse of the program
+ * prints the size of data types
+ * @void: Get's no parameter
+ * Return: 0 for perfect run or otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,3 +4,4 @@ We also made the assembly code not object code
 We make an executable C file from source codes
 We managed to create our first program 4-puts.c
 We print a message in a formated manner
+We printed the size of used data type
