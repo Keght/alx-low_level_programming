b3f2fc6ff958e672c3fe034e757db364f0ff51c8
NGABO Right Bruno <ngaborightbruno11@gmail.com>
2023-03-27 14:33:48 +0000

    print a formatted messaged

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+ * main - The intrance of our program
+ * @void: Receives no parameter
+ * Return: return 0 for correct or otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,3 +3,4 @@ I learnt to compile but don't link the c file
 We also made the assembly code not object code
 We make an executable C file from source codes
 We managed to create our first program 4-puts.c
+We print a message in a formated manner
