7d1a9cb9020e283be246dae21c2828803f1e2dbd
NGABO Right Bruno <ngaborightbruno11@gmail.com>
2023-03-28 15:12:46 +0000

    list base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
