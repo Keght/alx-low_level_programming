0901e7ea2efa00f95975b5444be64f99b4e1eb5f
NGABO Right Bruno <ngaborightbruno11@gmail.com>
2023-03-28 12:00:15 +0000

    print sign value of a number

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,14 @@ int main(void)
 	char str[] = "and that piece of art is useful\"";
 	char str1[] = "- Dora Korpar, 2015-10-19";
 	char str2[] = "\n";
+	int input[];
 
+	/* printing to the stand output*/
+	fwrite(str, sizeof(char), sizeof(str), stdout);
+	fwrite(str1, sizeof(char), sizeof(str1), stdout);
+	fwrite(&str2, sizeof(char), sizeof(str2), stdout);
+
+	/* printing to the standard error console */
 	fwrite(str, sizeof(char), sizeof(str), stderr);
 	fwrite(str1, sizeof(char), sizeof(str1), stderr);
 	fwrite(&str2, sizeof(char), sizeof(str2), stderr);
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Programming in C help me show the sign value of random number
