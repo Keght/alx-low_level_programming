d700522f3cd47213c85e567e12dda09dc3e7edd8
NGABO Right Bruno <ngaborightbruno11@gmail.com>
2023-05-01 13:44:55 +0000

    concatenate add n character of one string to another

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,55 @@
-#include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * string_nconcat - Concatenates two strings
+ * first string followed by n characters of second string
+ * @s1: First string.
+ * @s2: Second string.
+ * @n: Number of characters to copy from second string.
+ *
+ * Return: Pointer to formed string or NULL if function fails
+ */
 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j;
+	char *string;
+
+	/* Convert NULL pointer to empty string literals */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	/* Count number of characters in each string */
+	for (i = 0; *s1 != '\0'; i++)
+		s1++;
+
+	s1 = s1 - i;
+
+	for (j = 0; (*s2 != '\0') && (j < n); j++, i++)
+		s2++;
+
+	s2 = s2 - j;
+
+	/* Allocate memory to store the strings in concecutively */
+	string = malloc((i + 1) * sizeof(char));
+	if (string == NULL)
+	{
+		free(string);
+		return (NULL);
+	}
+
+	/* Copy first string to the memory allocated */
+	for (j = 0; *s1 != '\0'; j++, s1++)
+		string[j] = *s1;
+
+	/* Copy n characters of the second string to the remaining memory */
+	for (; j < i; j++, s2++)
+		string[j] = *s2;
+
+	string[j] = '\0';
+
+	return (string);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,8 @@
 #define MALLOC_CHECKED_H
 void *malloc_checked(unsigned int b);
 #endif
+
+#ifndef STRING_NCONCAT_H
+#define STRING_NCONCAT_H
+void *string_nconcat(char *s1, char *s2, unsigned int n);
+#endif
