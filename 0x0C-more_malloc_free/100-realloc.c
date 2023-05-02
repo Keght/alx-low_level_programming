2e0640f48927edf2847023d73637d70e23ffec07
NGABO Right Bruno <ngaborightbruno11@gmail.com>
2023-05-02 09:35:57 +0000

    works as realloc function to reallocate memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+
+void *allocator(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/**
+ * _realloc - Checks if data can be given new memory address:
+ * If checks passed it calls allocator to allocate the require memory,
+ * Copying the content in the previous memory.
+ * @ptr: The pointer to memory previously allocated with malloc
+ * @old_size: Size of the old previously allocated memory
+ * @new_size: New size of the memory.
+ *
+ * Return: NULL if ptr == NULL or if new_size == 0.
+ * Else, it returns pointer returned by allocator function.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newmem;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		else
+		{
+			newmem = malloc(new_size);
+			if (newmem == NULL)
+			{
+				free(newmem);
+				return (NULL);
+			}
+		}
+		return (newmem);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	return (allocator(ptr, old_size, new_size));
+}
+
+/**
+ * allocator - allocate the new memory form previous one.
+ * Copies the content in the previous memory and free it.
+ * If new_size > old_size, added part is not initialized.
+ *
+ * @ptr: The previously alocated memory
+ * @old_size: size of the previous alocated memory
+ * @new_size: size of new memory to be alocated.
+ *
+ * Return: Pointer to new memory or NULL if allocation failed
+ */
+
+void *allocator(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *mem;
+	char *oldmem;
+	void *newmem;
+
+	/* Allocate memory for new size */
+	newmem = malloc(new_size);
+	if (newmem == NULL)
+	{
+		free(newmem);
+		return (NULL);
+	}
+
+	/* Copy previous data to the new space */
+	mem = newmem;
+	oldmem = ptr;
+	for (new_size = 0; new_size < old_size; new_size++)
+	{
+		mem[new_size] = oldmem[new_size];
+	}
+
+	/* Free old memory and return new memory */
+	free(oldmem);
+	return (mem);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -22,3 +22,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 #define ARRAY_RANGE_H
 int *array_range(int min, int max);
 #endif
+
+#ifndef _REALLOC_H
+#define _REALLOC_H
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
